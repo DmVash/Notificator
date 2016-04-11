6f59728e3977afcadf860589c491c9298891f4ba
Дмитрий Вашека <d1ma2000@ukr.net>
2016-04-11 22:53:36 +0300

    Edit signup email notification

--- models/NotificationHandler.php
+++ models/NotificationHandler.php
@@ -17,12 +17,37 @@ class NotificationHandler extends Component
 
     public static function handleEmailNotification($event)
     {
+        $text = NotificationHandler::getNotificationText($event->data['code']);
+
         $message = Yii::$app->mailer->compose();
-        $message->setFrom('from@domain.com');
-        $message->setTo(Yii::$app->params['adminEmail'])
-            ->setSubject('Тема сообщения')
-            ->setTextBody('Текст сообщения')
+        $message->setFrom(Yii::$app->params['adminEmail']);
+        $message->setTo($event->data['email'])
+            ->setSubject($event->data['subject'])
+            ->setTextBody(NotificationHandler::replaceTextPattern($text, $event->data))
             ->send();
         //echo 'The notification has been successfully sent to the user' . $event->data['params'];
+    }
+
+    private function getNotificationText($code)
+    {
+        //return $text = $this->findOne(['code' => $code])->text;
+        $notification = new Notifications();
+        return $notification->getNotificationText($code);
+    }
+
+    private function replaceTextPattern($text, $params)
+    {
+    //Todo можно сделать получение набора шаблонов из базы
+       $patterns = ['{username}','{sitename}','{articleName}','{shortText}'];
+        $replacedText = '';
+        //foreach($patterns as $pattern){
+
+            if(strpos($text, '{username}'))
+                $text = str_replace('{username}', $params['username'], $text);
+            if(strpos($text, '{sitename}'))
+                $text  = str_replace('{sitename}', Yii::$app->params['siteName'], $text);
+                        //}
+
+        return $text;
     }
 }
