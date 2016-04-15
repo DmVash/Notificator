7f4cc7c2384130c72de3efaa4c178478e9d7bccd
DmVash <d1ma2000@ukr.net>
2016-04-15 17:45:08 +0300

    Development notification form

--- views/notifications/_form.php
+++ views/notifications/_form.php
@@ -2,10 +2,16 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\User;
+use \yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\SendingBrowserNotifications */
+/* @var $model app\models\SendingNotifications */
 /* @var $form yii\widgets\ActiveForm */
+
+$users = User::find()->all();
+$items = ArrayHelper::map($users, 'id', 'username');
+
 ?>
 
 <div class="sending-browser-notifications-form">
@@ -22,8 +28,12 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'user_id')->textInput() ?>
 
+    <?= $form->field($model, 'type')->dropDownList(['browser'=>'Browser', 'email'=>'Email'], ['multiple' => 'true']) ?>
+
+
+
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Send Notification') : Yii::t('app', 'Resend Notification'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
